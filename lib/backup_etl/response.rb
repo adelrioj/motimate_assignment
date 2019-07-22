ef3d7470f5a8b1d570e7904a1f5f6998f1ac1ded
adelrioj <alejandro@nextail.co>
2019-07-23 00:43:16 +0200

    add between? method to Response

--- lib/backup_etl/response.rb
+++ lib/backup_etl/response.rb
@@ -14,6 +14,11 @@ module BackupEtl
       @error_description = error_description
     end
 
+    def between?(interval:)
+      @created_at > interval.start_time &&
+        @created_at < interval.end_time
+    end
+
     def to_a
       [@id, @created_at.to_s, @status, @code, @error_description]
     end
---
+++ spec/backup_etl/response_spec.rb
@@ -0,0 +1,38 @@
+require "spec_helper"
+
+RSpec.describe BackupEtl::Response do
+  let(:response) do
+    BackupEtl::Response.new(
+      id: 'an_id',
+      created_at: Time.parse('2019-04-09 00:06:19 +0000'),
+      code: 'a_code',
+      status: 'a_status'
+    )
+  end
+
+  context 'between?' do
+    it 'should return true if created_at is between interval' do
+      interval = BackupEtl::Interval.new(
+        start_time: Time.parse('2019-04-08 00:06:19 +0000'),
+        end_time: Time.parse('2019-04-10 00:08:02 +0000')
+      )
+      expect(response.between?(interval: interval)).to be true
+    end
+
+    it 'should return false if created_at is below interval' do
+      interval = BackupEtl::Interval.new(
+        start_time: Time.parse('2019-04-10 00:06:19 +0000'),
+        end_time: Time.parse('2019-04-10 00:08:02 +0000')
+      )
+      expect(response.between?(interval: interval)).to be false
+    end
+
+    it 'should return false if created_at is above interval' do
+      interval = BackupEtl::Interval.new(
+        start_time: Time.parse('2019-04-08 00:06:19 +0000'),
+        end_time: Time.parse('2019-04-8 00:08:02 +0000')
+      )
+      expect(response.between?(interval: interval)).to be false
+    end
+  end
+end
